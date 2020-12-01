7e687c98657f2f97f10b6c32a2f64986256ffca3
Callum Latimer <cm_latimer-1@yahoo.co.uk>
2020-12-01 18:21:43 +0000

    Day 1 Initial

--- Day1.cpp
+++ Day1.cpp
@@ -1,52 +1,51 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
+// Part 1
 // [[Rcpp::export]]
-int divideTwoIfEven(int x) {
-  if(x % 2 == 0) {
-    return x/2;
-  } else {
-    return x;
+int part1(Rcpp::NumericVector x, int val = 2020){
+  x.sort();
+  while (x.length() > 2){
+    x = x[x <= val - x[0]];
+    if(x[0] + x[x.length() - 1] == val){
+      Rcpp::NumericVector V = Rcpp::NumericVector {x[0], x[x.length() - 1]};
+      return(V[0]*V[1]);
+    } else {
+      x = x[x > x[0]];
+    }
   }
+  return 0;
 }
-
+ 
+// Part 2
 // [[Rcpp::export]]
-Rcpp::IntegerVector divideTwoIfEvenVector(Rcpp::IntegerVector x) {
-  return sapply(x, divideTwoIfEven);
-}
-
-// [[Rcpp::export]]
-int addOneIfOdd(int x) {
-  if(x % 2 != 0) {
-    return x + 1;
-  } else {
-    return x;
+int part2(Rcpp::NumericVector x, int val = 2020){
+  x.sort();
+  while (x.length() > 3){
+    x = x[x <= val - x[1] - x[0]];
+    int i = 1;
+    while(x.length() - i > 0){
+      Rcpp::NumericVector diff = val - x[x.length() - i] - x[0] - x;
+      if(any(diff == 0).is_true()){
+        Rcpp::NumericVector V = Rcpp::NumericVector {x[0], x[x.length() - i], val - x[x.length() - i] - x[0]};
+        return(V[0]*V[1]*V[2]);
+      } else {
+        i++;
+      }
+    }
+    x = x[x > x[0]];
   }
+  return 0;
 }
-
-// [[Rcpp::export]]
-Rcpp::IntegerVector addOneIfOddVector(Rcpp::IntegerVector x) {
-  return sapply(x, addOneIfOdd);
-}
-
-
-// [[Rcpp::export]]
-int divideTenIfTens(int x){
-  if(x % 10 == 0) {
-    return x / 10;
-  } else {
-    return x;
-  }
-}
-
-// [[Rcpp::export]]
-Rcpp::IntegerVector divideTenIfTensVector(Rcpp::IntegerVector x) {
-  return sapply(x, divideTenIfTens);
-}
-
-
+  
 /*** R
-divideTwoIfEvenVector(seq(1:100))
-addOneIfOddVector(seq(1:100))
-divideTenIfTensVector(seq(1:100))
+data = scan("Day1.txt")
+part1(data)
+part2(data)
+
+library(microbenchmark)
+microbenchmark(part1(data), times = 100000L)
+microbenchmark(part2(data), times = 100000L)
 */
+
+
