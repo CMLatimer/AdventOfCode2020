c42f5fd5cc8d63dbb7026c5fc3d46978abb4a2eb
Callum Latimer <cm_latimer-1@yahoo.co.uk>
2020-12-02 13:18:46 +0000

    Day 2 Part 2

--- Day2.cpp
+++ Day2.cpp
@@ -21,12 +21,31 @@ int part1(std::vector<std::string> strings){
   return result;
 }
 
+// Part 2
+// [[Rcpp::export]]
+int part2(std::vector<std::string> strings){
+  int num_strings = strings.size();
+  int result = 0;
+  
+  for(int i = 0; i < num_strings; i++){
+    int first = std::stoi(strings[i].substr(0,strings[i].find('-')));
+    int second = std::stoi(strings[i].substr(strings[i].find('-')+1, strings[i].find(' ') - strings[i].find('-') - 1));
+    char letter = strings[i].substr(strings[i].find(' ')+1,strings[i].find(':') - strings[i].find(' ') - 1)[0];
+    std::string password = strings[i].substr(strings[i].find(':')+2, std::string::npos);
+    if((password[first-1] == letter)^(password[second-1] == letter)){
+      result++;
+    }
+  }
+  return result;
+}
+
 /*** R
-#data <- c(
-#   '1-3 a: abcde'
-#  ,'1-3 b: cdefg'
-#  ,'2-9 c: ccccccccc'
-#)
+#data <- c('1-3 a: abcde','1-3 b: cdefg','2-9 c: ccccccccc')
 data <- scan('Day2.txt', sep = '\n', what = character())
 part1(data)
+part2(data)
+
+library(microbenchmark)
+microbenchmark(part1(data), times = 100000L)
+microbenchmark(part2(data), times = 100000L)
 */
